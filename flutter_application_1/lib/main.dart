847b42ed0560803ae008bdf786670e28d0c0d517
unknown <doubienoe0@gmail.com>
2024-07-25 16:42:28 +0100

    white background

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -77,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
